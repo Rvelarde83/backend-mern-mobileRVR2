460efb4bf27977a9441698d858b653c634f5c974
Ramon Velarde <rvelarde83@hotmail.com>
2022-06-06 23:14:31 -0700

    s

--- server.js
+++ server.js
@@ -256,7 +256,7 @@ app.post("/babies/:id/feeding", async (req, res) => {
 //Create2
 app.post("/feeding", async (req, res) => {
     try {
-        res.json(await FEeding.create(req.body))
+        res.json(await Feeding.create(req.body))
     } catch (error) {
         res.status(400).json(error)
     }
